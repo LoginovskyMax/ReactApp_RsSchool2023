2169277ed4aec38328a8dd6b58ede3d22757ac58
Loginovsky Max <104369728+LoginovskyMax@users.noreply.github.com>
2023-03-13 19:44:21 +0600

    feat:components

---
+++ src/Components/Card.tsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import styles from './Card.module.scss';
+import { IProduct } from '../Pages/MainPage';
+
+interface IProps {
+  data: IProduct;
+}
+interface IState {
+  visible: boolean;
+}
+
+class Card extends Component<IProps, IState> {
+  data: IProduct;
+  constructor(props: IProps) {
+    super(props);
+    this.data = props.data;
+    this.state = { visible: false };
+  }
+
+  render() {
+    return (
+      <div className={styles.card}>
+        <img src={this.data.thumbnail} alt="img" width="200" />
+        <p>
+          <strong>Brand :</strong>
+          {this.data.brand}
+        </p>
+        <p>
+          <strong>Category :</strong>
+          {this.data.category}
+        </p>
+        <p
+          onMouseEnter={() => this.setState({ visible: true })}
+          onMouseLeave={() => this.setState({ visible: false })}
+          className={styles.card__descriptor}
+        >
+          Description
+        </p>
+        {this.state.visible && <p className={styles.card__description}>{this.data.description}</p>}
+        <p>
+          <strong>Price: </strong>
+          {this.data.price} $
+        </p>
+        <p>
+          <strong>Rating:</strong> {this.data.rating}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Card;
